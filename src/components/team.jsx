ab0c3586e5b0dc7a2c08100e2f8a55d76f2e3bec
Sahil Kumar Gupta <g.sahil@iitg.ac.in>
2023-03-15 18:36:04 +0530

    team-init

---
+++ src/components/team.jsx
@@ -0,0 +1,7 @@
+export default function Team() {
+  return (
+    <div className='grid place-content-center gap-10 justify-items-center h-screen'>
+      <h1>Hii</h1>
+    </div>
+  );
+}
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,4 +1,5 @@
 import { Link, useRouteError } from "react-router-dom";
+import Team from "../components/team";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -6,17 +7,7 @@ export default function ErrorPage() {
 
   return (
     <div className='grid place-content-center gap-10 justify-items-center h-screen'>
-      <h1 className='text-4xl font-bold'>Oops!</h1>
-      <p className='text-lg'>Sorry, an unexpected error has occurred.</p>
-      <p className='text-gray-500'>
-        <i>{error.statusText || error.message}</i>
-        <i> ({error.status}) </i>
-      </p>
-      <Link to='/'>
-        <button className='px-4 py-2 text-black italic bg-gray-200 rounded'>
-          Go Home
-        </button>
-      </Link>
+      <Team/>
     </div>
   );
 }
