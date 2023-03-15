de27d1fc9df347291169f1d7d07f2ad881d9e65a
Sahil Kumar Gupta <g.sahil@iitg.ac.in>
2023-03-16 02:43:00 +0530

    team-component-partial

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        customBlue: {
+          100: "#0094FF",
+          200: "#0050FF",
+          300: "#032538",
+        },
+        customOrange: {
+          100: "#FF703C"
+        },
+      },
+    },
+    
   },
   plugins: [],
 }
