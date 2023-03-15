b7bda92de473cd403fa48b9400463463bc8c5dec
Aman Kumar <aman.26262@gmail.com>
2023-03-15 18:17:37 +0530

    tailwind and vite setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <img src={reactLogo} className="h-24" alt="React Logo" />
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
