435a873bac13ff5dae1a02b5861898c4a68fde81
Aman Kumar <aman.26262@gmail.com>
2023-03-20 01:37:27 +0530

    work

--- src/components/project-description.jsx
+++ src/components/project-description.jsx
@@ -1,49 +1,90 @@
-import React from "react"
-import Cross from "../assets/Cross-Line.png"
+import React from "react";
+import Cross from "../assets/Cross-Line.png";
+import Raman from "../assets/raman.png";
+import Ball from "./ball";
 
-export default function ProjectDescription({isVisible, onClose, data}) {
-    if(!isVisible) return null;
-    const handleClose = (e) =>{
-        if(e.target.id === 'wrapper')onClose();
-    }
-    return(
-        <div className="fixed inset-0 bg-black bg-opacity-25
-        backdrop-blur-sm flex justify-center items-center " id="wrapper"
-        onClick={handleClose} style={{transition: "right 1s ease-in-out"}}>
-            <div className='flex w-11/12 h-5/6 text-white shadow-2xl'>
-                <div className='w-full md:w-7/12 bg-customBlue-200 items-center text-left '>
-                    <div className='h-5/6 w-9/12 md:w-10/12 lg:w-8/12 items-center mx-[15%] md:mx-[5%] lg:md-[10%] mt-16 md:mt-8'>
-                        <img className="hover:cursor-pointer h-4 md:h-6" onClick={() => onClose()} src={Cross}/>
-                        <br/>
-                        <div className="flex mb-2 md:mb-6">
-                            <p className='text-2xl md:text-5xl font-semibold text-white -tracking-[0.01em] leading-tight font-body mr-2 md:mr-4'>
-                                {data.project_name}
-                            </p>
-                            <div className="h-8 md:h-12" style={{borderLeft: '1px solid white'}}></div>
-                            <div className="ml-2 md:ml-4 -mt-1 md:-mt-2">
-                                <p className="text-xs md:text-sm">Stall No.</p>
-                                <p className="text:2xl md:text-4xl font-semibold">{data.stall_no}</p>
-                            </div>
-                        </div>
-                        <p className='font-semibold md:text-base md:leading-4 mb-2'>About Project</p>
-                        <p className='text-sm md:text-base mb-2 md:mb-4'>{data.about}</p>
-                        <p className='font-semibold md:text-base md:leading-6 mb-2'>Other Details</p>
-                        <p className='text-sm md:text-base mb-4 md:mb-4'>{data.other_details}</p>
-                        <div className="flex items-center h-10 md:h-12">
-                            <img className='h-full' src={data.img_url}/>
-                            <div className='ml-4'>
-                                <p className='font-body font-semibold text-base md:leading-4 -tracking-[0.01em]'>{data.club_name}</p>
-                                <p className='text-sm'>IIT Guwahati</p>
-                            </div>
-                        </div>
-                        <div className="flex mt-6 mb-6" >
-                            <p className='font-semibold text-base leading-6 mr-4'>Team:</p>
-                            <p className='text-base'>{data.team}</p>
-                        </div>
-                        <div className="bg-white w-48 md:w-64 h-12 rounded-3xl"></div>
-                    </div>
-                </div>
+export default function ProjectDescription({ isVisible, onClose, data }) {
+  if (!isVisible) return null;
+  const handleClose = (e) => {
+    if (e.target.id === "wrapper") onClose();
+  };
+  return (
+    <div
+      className='fixed inset-0 bg-black bg-opacity-25
+        backdrop-blur-sm flex justify-center items-center '
+      id='wrapper'
+      onClick={handleClose}
+      style={{ transition: "right 1s ease-in-out" }}
+    >
+      <div className='flex w-11/12 h-5/6 text-white shadow-2xl'>
+        <div className='w-full md:w-7/12 bg-customBlue-200 items-center text-left '>
+          <div className='h-5/6 w-9/12 md:w-10/12 lg:w-8/12 items-center mx-[15%] md:mx-[5%] lg:md-[10%] mt-16 md:mt-8'>
+            <img
+              className='hover:cursor-pointer h-4 md:h-6'
+              onClick={() => onClose()}
+              src={Cross}
+            />
+            <br />
+            <div className='flex mb-2 md:mb-6'>
+              <p className='text-2xl md:text-5xl font-semibold text-white -tracking-[0.01em] leading-tight font-body mr-2 md:mr-4'>
+                {data.project_name}
+              </p>
+              <div
+                className='h-8 md:h-12'
+                style={{ borderLeft: "1px solid white" }}
+              ></div>
+              <div className='ml-2 md:ml-4 -mt-1 md:-mt-2'>
+                <p className='text-xs md:text-sm'>Stall No.</p>
+                <p className='text:2xl md:text-4xl font-semibold'>
+                  {data.stall_no}
+                </p>
+              </div>
             </div>
+            <p className='font-semibold md:text-base md:leading-4 mb-2'>
+              About Project
+            </p>
+            <p className='text-sm md:text-base mb-2 md:mb-4'>{data.about}</p>
+            <p className='font-semibold md:text-base md:leading-6 mb-2'>
+              Other Details
+            </p>
+            <p className='text-sm md:text-base mb-4 md:mb-4'>
+              {data.other_details}
+            </p>
+            <div className='flex items-center h-10 md:h-12'>
+              <img className='h-full' src={data.img_url} />
+              <div className='ml-4'>
+                <p className='font-body font-semibold text-base md:leading-4 -tracking-[0.01em]'>
+                  {data.club_name}
+                </p>
+                <p className='text-sm'>IIT Guwahati</p>
+              </div>
+            </div>
+            <div className='flex mt-6 mb-6'>
+              <p className='font-semibold text-base leading-6 mr-4'>Team:</p>
+              <p className='text-base'>{data.team}</p>
+            </div>
+            <div className='bg-white w-48 md:w-64 h-12 rounded-3xl'></div>
+          </div>
         </div>
-    )
+        <div className="bg-white w-0 md:w-5/12">
+          <div className='mx-12 flex h-full relative overflow-hidden'>
+            <div className='absolute top-10 left-0 z-20'>
+              <Ball image={Raman} />
+            </div>
+            <div className='absolute top-10 right-0 z-20'>
+              <Ball image={Raman} />
+            </div>
+            <div className='absolute bottom-40 left-32 z-20'>
+              <Ball image={Raman} />
+            </div>
+            <div className='self-end mb-20 bg-customBlue-200 rounded-full w-full p-5 flex justify-center items-center text-white'>
+              <p className='font-ClashDisplay text-2xl font-bold'>
+                Vote Now
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/routes/hardware-info.jsx
+++ src/routes/hardware-info.jsx
@@ -1,22 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Grid from "../assets/Group 6.svg";
+import Ball from "../components/ball";
+import Raman from "../assets/raman.png";
 
 export default function HardwareInfo() {
-    return(
-        <div className='flex'>
-            <div className='w-full md:w-5/12 bg-customBlue-200 h-screen items-center text-left'>
-            <div className='h-5/6 w-8/12 items-center mx-auto my-24'>
-                <p className='text-3xl md:text-5xl font-semibold text-white -tracking-[0.01em] leading-tight font-body'>
-                    Hardware<br/>
-                    Clubs
-                </p>
-                <br/><br/>
-                <p className='text-white text-base leading-6'>
-                    With the unique ability to create advanced machines that make human life so much easier, 
-                    no one holds a light to IITG's hardware clubs. This Techevince, with projects like Yuvaan and Antha Sagri, 
-                    our clubs promise to engineer brilliance through passion, innovation, and curiosity.
-                </p>
-            </div>
-            </div>
+  return (
+    <div className='flex bg-white z-0'>
+      <div className='w-full md:w-5/12 bg-customBlue-200 h-screen items-center text-left'>
+        <div className='h-5/6 w-8/12 relative items-center mx-auto my-24'>
+          <div className='absolute -bottom-5 overflow-hidden -left-32 w-[160%] h-2/5'>
+            <div className='bg-gradient-to-t from-transparent z-20 to-customBlue-200 w-full h-full absolute bottom-0 left-0'></div>
+            <div
+              style={{ background: `url("${Grid}") no-repeat` }}
+              className='absolute bottom-0 left-0 z-10 w-full h-full'
+            />
+          </div>
+          <p className='text-3xl md:text-5xl font-semibold text-white -tracking-[0.01em] leading-tight font-body'>
+            Hardware
+            <br />
+            Clubs
+          </p>
+          <br />
+          <br />
+          <p className='text-white text-base leading-6'>
+            With the unique ability to create advanced machines that make human
+            life so much easier, no one holds a light to IITG's hardware clubs.
+            This Techevince, with projects like Yuvaan and Antha Sagri, our
+            clubs promise to engineer brilliance through passion, innovation,
+            and curiosity.
+          </p>
         </div>
-    )
+      </div>
+      <div className='z-30 md:w-7/12 w-0 bg-white'>
+        <div className='mx-12 flex h-full relative overflow-hidden'>
+          <div className='absolute top-10 left-0 z-20 bounce'>
+            <Ball image={Raman} />
+          </div>
+          <div className='absolute top-10 right-0 z-20 bounce-dia'>
+            <Ball image={Raman} />
+          </div>
+          <div className='absolute top-10 left-0 z-20 bounce-rotate'>
+            <Ball image={Raman} />
+          </div>
+          <div className='self-end mb-20 bg-customBlue-200 rounded-full w-full p-5 flex justify-between items-center text-white'>
+            <p className='font-ClashDisplay text-2xl font-bold'>
+              Vote Projects
+            </p>
+            <div className='border-2 border-white rounded-full p-3 bg-[#333333] font-semibold'>
+              👆🏻Press and hold project to open
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
